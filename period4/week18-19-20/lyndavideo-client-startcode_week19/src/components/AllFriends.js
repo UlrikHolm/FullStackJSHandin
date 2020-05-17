037ba69cf331700eb461eb1c7393a07463bfb8df
Ulrik Holm <ulrikholmuh@gmail.com>
2020-05-17 03:15:32 +0200

    period4

---
+++ period4/week18-19-20/lyndavideo-client-startcode_week19/src/components/AllFriends.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks"
+import { gql } from "apollo-boost"
+
+const ALL_FRIENDS = gql`
+{
+  getAllFriends {
+    id
+    firstName
+    lastName
+    language
+    gender
+    age
+    email
+  }
+}
+`
+
+export default function All() {
+  const { loading, error, data, networkStatus } = useQuery(ALL_FRIENDS);
+  //const { loading, error, data, networkStatus } = useQuery(ALL_FRIENDS, { fetchPolicy: "no-cache" });
+  //const { loading, error, data, networkStatus } = useQuery(ALL_FRIENDS, { pollInterval: 15000 });
+  if (loading) return (<h3>Loading...</h3>)
+  if (error) return <p> {JSON.stringify(error)}</p>
+  if (!data) return <p>No Data</p>
+ 
+  return data.getAllFriends.map(f => {
+    const age = f.age ? `, Age: ${f.age}` : null;
+    return <p key={f.id}>{f.id}, {f.firstName} {f.lastName}, {f.gender}, {f.email} {age} </p>
+  })
+}
---
+++ period4/week18-19-20/lyndavideo-client-startcode_week20/src/components/AddFriend.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { useQuery,useMutation } from "@apollo/react-hooks"
+import { gql } from "apollo-boost"
+
+const ALL_FRIENDS = gql`
+{
+  getAllFriends{
+    id
+    firstName
+    lastName
+    gender
+    age
+  }
+}
+`
+const ADD_FRIEND = gql`
+  mutation 
+    createFriend($input:FriendInput!) {
+      createFriend(input: $input){
+        id
+        firstName
+        lastName
+        gender
+        age
+      }
+    }
+  `
+
+
+
+const AddFriend = ({ initialFriend, allowEdit }) => {
+  const EMPTY_FRIEND = { firstName: "", lastName: "", gender: "OTHER", age: "", email: "" }
+  let newFriend = initialFriend ? initialFriend : { ...EMPTY_FRIEND }
+
+  const [friend, setFriend] = useState({ ...newFriend })
+  const [readOnly, setReadOnly] = useState(!allowEdit)
+
+const [createFriend, {data}] = useMutation(ADD_FRIEND,
+  {
+    update(cache, { data }) {
+      const newFriend = data.createFriend;
+      const p = cache.readQuery({ query: ALL_FRIENDS })
+      const { allFriends } = p;
+      allFriends.push(newFriend)
+      cache.writeQuery({
+        query: ALL_FRIENDS,
+        data: { allFriends: [...allFriends] }
+      });
+    }
+  }
+)
+  const handleChange = (event) => {
+    const id = event.target.id;
+    friend[id] = event.target.value;
+    setFriend({ ...friend })
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    
+
+    createFriend({
+      variables: {
+        input: {
+          id: friend.id,
+          firstName: friend.firstName,
+          lastName: friend.lastName,
+          email: friend.email,
+          age: Number(friend.age),
+          gender: friend.gender
+        }
+      }
+    }
+    )
+
+    setFriend({ ...EMPTY_FRIEND })
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        FirstName<br/>
+        <input type="text" readOnly={readOnly} id="firstName" value={friend.firstName} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+       LastName <br/>
+        <input readOnly={readOnly} type="text" id="lastName" value={friend.lastName} onChange={handleChange} />
+      </label>
+      <br />
+        <label>
+          Gender &nbsp;
+          <select disabled={readOnly} id="gender" value={friend.gender} onChange={handleChange}>
+            <option value="MALE">Male</option>
+            <option value="FEMALE">Female</option>
+            <option value="OTHER">Other</option>
+          </select>
+        </label>
+      <br />
+      <label>
+        Age <br/>
+          <input readOnly={readOnly} type="number" id="age" value={friend.age} onChange={handleChange} />
+      </label>
+      <br /><br/>
+      {!readOnly && <input type="submit" value="Submit" />}
+    </form>
+  );
+}
+
+export default AddFriend;
