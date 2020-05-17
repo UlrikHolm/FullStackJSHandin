67b83b564e7ec56331004e2a7d76a9283c0cc2c6
Ulrik Holm <ulrikholmuh@gmail.com>
2020-05-17 03:23:07 +0200

    geo_graphql

---
+++ period4/week18-19-20/startcodeForP2_Day6_MongoGeographQL/src/routes/graphqlAPI.ts
@@ -0,0 +1,98 @@
+import express from "express";
+import userFacade from "../facades/userFacadeWithDB";
+import basicAuth from "../middlewares/basic-auth"
+const router = express.Router();
+import { ApiError } from "../errors/apiError"
+//import * as mongo from "mongodb"
+import setup from "../config/setupDB"
+//const MongoClient = mongo.MongoClient;
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+import GameUser from "../interfaces/GameUser"
+
+
+const USE_AUTHENTICATION = true;
+
+const schema = buildSchema(`
+  type User {
+    name: String
+    userName: String
+    role: String
+    password: String
+  }
+ 
+  input UserInput {
+    name: String
+    userName: String
+    password: String
+  }
+  
+  type Query {
+    users : [User]!
+  }
+  type Mutation {
+    createUser(input: UserInput): String
+  }
+`)
+
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    users: async () => {
+        const users = await userFacade.getAllUsers();
+        const usersDTO = users.map((user) => {
+            const { name, userName, role } = user;
+            return { name, userName, role }
+        })
+        return usersDTO;
+    },
+    createUser: async (inp: any) => {
+        const { input } = inp;
+        try {
+            const newUser: GameUser = {
+                name: input.name,
+                userName: input.userName,
+                password: input.password,
+                role: "user"
+            }
+
+            const status = await userFacade.addUser(newUser)
+            return status;
+
+        } catch (err) {
+            throw err;
+        }
+    }
+
+};
+
+if (USE_AUTHENTICATION) {
+    router.use(basicAuth)
+  }
+   
+//  Only if we need roles
+  router.use("/", (req: any, res, next) => {
+    if (USE_AUTHENTICATION) {
+      const role = req.role;
+      if (role != "admin") {
+        throw new ApiError("Not Authorized", 403)
+      }
+      next();
+    }
+  })
+
+  router.use('/', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+(async function setupDB() {
+    const client = await setup()
+    userFacade.setDatabase(client)
+})()
+
+
+  
+
+module.exports = router;
